55e86ab1f2d48c806c150b8e89241b3618b7df9e
falcon71181 <abhaythakur71181@gmail.com>
2024-12-14 15:05:55 +0530

    feat: async watch fn to create watcher event listener & added addtitonal errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
-use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
+use std::sync::mpsc;
 
 use config::ConfigError;
-use log::{debug, info, trace, warn, error};
+use git2::Repository;
+use log::{debug, error, info, trace, warn};
+use notify::Event;
+use notify::EventKind;
+use notify::RecursiveMode;
 use serde::Deserialize;
 use serde::Serialize;
 use thiserror::Error;
+use tokio::task;
+use tokio::task::JoinError;
 
 mod config;
+mod git;
+mod helper;
 mod logger;
 
 /// Represents the Git Auto Pilot configuration and file management
@@ -45,6 +53,14 @@ pub enum GitAutoPilotError {
     /// Wrapper for standard IO errors
     #[error(transparent)]
     IOError(#[from] std::io::Error),
+
+    /// Wrapper for standard notify errors
+    #[error(transparent)]
+    NotifyError(#[from] notify::Error),
+
+    /// Wrapper for standard tokio join errors
+    #[error(transparent)]
+    TokioJoinError(#[from] JoinError),
 }
 
 // Log the error details when the GitAutoPilotError is being dropped
@@ -87,6 +103,78 @@ impl GitAutoPilot {
             dot_file_location: dot_file,
         })
     }
+
+    /// Watches file system changes in specified repositories and processes the events.
+    ///
+    /// # Arguments
+    /// - `self` - The current instance of the struct containing configuration and other details.
+    ///
+    /// # Returns
+    /// - `Result<(), GitAutoPilotError>` - Returns `Ok(())` if successful, otherwise an error of type `GitAutoPilotError`.
+    ///
+    /// # Behavior
+    /// 1. Creates a standard library channel and a Tokio channel for event handling.
+    /// 2. Configures a file watcher for directories specified in the configuration.
+    /// 3. Bridges events from the standard channel to the Tokio channel.
+    /// 4. Processes events asynchronously to handle file system changes.
+    ///
+    /// # Errors
+    /// - Returns an error if the watcher setup or event processing fails.
+    pub async fn watch(self) -> Result<(), GitAutoPilotError> {
+        trace!("Starting watch function...");
+
+        // Create a standard library channel for file system events
+        let (tx, rx) = mpsc::channel();
+
+        // Tokio channel for async processing
+        let (async_tx, mut async_rx) = tokio::sync::mpsc::channel(100);
+
+        // Configure watcher
+        let mut watcher = helper::create_watcher(tx)?;
+
+        // Directories to watch
+        let watch_paths = &self.config.repos;
+
+        // Watch multiple directories
+        for path in watch_paths {
+            info!("Adding watch for path: {:#?}", path);
+            watcher.watch(Path::new(path), RecursiveMode::Recursive)?;
+        }
+
+        // Spawn a task to bridge standard channel to Tokio channel
+        let bridge_handle = task::spawn(async move {
+            for event in rx {
+                trace!("Received event: {:?}", event);
+                if let Err(_) = async_tx.send(event).await {
+                    error!("Failed to send event through async channel");
+                    break;
+                }
+            }
+        });
+
+        // Process events
+        while let Some(result) = async_rx.recv().await {
+            match result {
+                Ok(event) => {
+                    debug!("Handling event: {:?}", event);
+                    trace!("Finding correct repo that triggered event");
+                    if let Some(repo) = get_matching_repository(&event.paths[0], &self.config.repos)
+                    {
+                        debug!("Matched repository for event: {:?}", repo);
+                        handle_event(&event, &repo);
+                    } else {
+                        debug!("No matching repository found for paths: {:?}", event.paths);
+                    }
+                }
+                Err(e) => error!("Watch error: {:?}", e),
+            }
+        }
+
+        // Wait for the bridge task to complete
+        bridge_handle.await?;
+        info!("Watch function completed successfully.");
+        Ok(())
+    }
 }
 
 /// Determines the path for the dot directory
@@ -99,16 +187,20 @@ impl GitAutoPilot {
 fn get_dot_dir_path() -> Result<String, GitAutoPilotError> {
     trace!("Attempting to retrieve home directory");
 
-    // Prefer dirs crate's home_dir, fallback to environment variable
-    let dot_dir = dir::home_dir()
+    let home_dir_from_dirs = dir::home_dir();
+
+    let dot_dir = home_dir_from_dirs
         .map(|path| format!("{}/{}", path.display(), DOT_DIR))
         .or_else(|| {
             warn!("Could not retrieve home directory via dirs");
-            env::var("HOME")
+            std::env::var("HOME")
                 .map(|home| format!("{}/{}", home, DOT_DIR))
                 .ok()
         })
-        .ok_or(GitAutoPilotError::HomeDirError)?;
+        .ok_or_else(|| {
+            error!("Failed to determine home directory through both dirs and HOME environment variable");
+            GitAutoPilotError::HomeDirError
+        })?;
 
     trace!("Home directory for dot directory determined: {}", dot_dir);
     Ok(dot_dir)
@@ -168,4 +260,50 @@ fn load_or_create_config(dot_file: &str) -> Result<config::Config, GitAutoPilotE
 
         config::Config::load_from_file(&config_path).map_err(|e| GitAutoPilotError::ConfigError(e))
     }
+}
+
+/// Handles a single file system event by analyzing changes in the corresponding Git repository.
+///
+/// # Arguments
+/// - `event` - The file system event to be handled.
+/// - `repo` - The path to the Git repository related to the event.
+///
+/// # Behavior
+/// - Analyzes repository changes for specified file paths.
+/// - Logs detailed information about the changes.
+fn handle_event(event: &Event, repo: &Path) {
+    match event.kind {
+        EventKind::Create(_) | EventKind::Modify(_) | EventKind::Remove(_) => {
+            for path in &event.paths {
+                trace!("Path  - {}", path.display());
+                info!(
+                    "{:#?}",
+                    git::analyze_repository_changes(&Repository::open(repo).unwrap())
+                );
+            }
+        }
+        _ => {}
+    }
+}
+
+/// Finds the repository that matches a given file path.
+///
+/// # Arguments
+/// - `path` - The file system path to match.
+/// - `repos` - A list of repository paths to search.
+///
+/// # Returns
+/// - `Option<&Path>` - Returns a reference to the matching repository path, or `None` if no match is found.
+///
+/// # Behavior
+/// - Checks if the given path is contained within any of the repository paths.
+fn get_matching_repository<P: AsRef<Path>>(path: P, repos: &[PathBuf]) -> Option<&Path> {
+    repos
+        .iter()
+        .find(|r| {
+            r.to_str().map_or(false, |r_str| {
+                path.as_ref().to_string_lossy().contains(r_str)
+            })
+        })
+        .map(|r| r.as_path())
 }
