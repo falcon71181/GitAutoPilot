8ea6b9784ed6c221fb24d26eba0f4a0a87b59c1c
falcon71181 <abhaythakur71181@gmail.com>
2024-12-16 09:08:57 +0530

    feat: main rs instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use git_auto_pilot::GitAutoPilot;
+
+mod config;
+mod git;
+mod helper;
+mod logger;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cmd_arguments = clap::Command::new("cmd-program")
+        .arg(
+            clap::Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .action(clap::ArgAction::Count) // This is the new way to count occurrences
+                .help("Increases logging verbosity each use for up to 3 times"),
+        )
+        .get_matches();
+
+    // Get the number of times the verbose flag was passed
+    let verbosity: u64 = cmd_arguments.get_count("verbose") as u64;
+
+    let git_auto_pilot = GitAutoPilot::new(verbosity)?;
+    GitAutoPilot::watch(git_auto_pilot).await?;
+    Ok(())
 }
