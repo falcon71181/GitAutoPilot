97d5e7b9a7134a32ae5771e8e91f516582d60f4b
falcon71181 <abhaythakur71181@gmail.com>
2024-12-27 21:40:05 +0530

    feat: take action to commit changes and push to origin

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 use std::fs;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc;
 
-use config::{ConfigError, GitCred};
+use config::{ConfigError, Message, SYSTEM_VARIABLES};
 use error::GitAutoPilotError;
 use git::FileChangeStats;
 use git2::{Repository, Status};
@@ -105,7 +106,7 @@ impl GitAutoPilot {
         let watch_paths = &self.config.repos;
 
         // Ignored directories
-        let ignored_dirs: Vec<String> = self.config.ignored_dirs;
+        let ignored_dirs: &Vec<String> = &self.config.ignored_dirs;
 
         // Watch multiple directories
         for path in watch_paths {
@@ -144,7 +145,7 @@ impl GitAutoPilot {
                         helper::get_matching_repository(&event.paths[0], &self.config.repos)
                     {
                         debug!("Matched repository for event: {:?}", repo);
-                        let _ = handle_event(&event, &repo, &self.config.git_credentials);
+                        let _ = Self::handle_event(&self, &event, &repo);
                     } else {
                         debug!("No matching repository found for paths: {:?}", event.paths);
                     }
@@ -157,6 +158,278 @@ impl GitAutoPilot {
         bridge_handle.await?;
         info!("Watch function completed successfully.");
         Ok(())
+    }
+
+    /// Handles a single file system event by analyzing changes in the corresponding Git repository.
+    ///
+    /// # Arguments
+    /// - `event` - The file system event to be handled.
+    /// - `repo` - The path to the Git repository related to the event.
+    ///
+    /// # Behavior
+    /// - Analyzes repository changes for specified file paths.
+    /// - Logs detailed information about the changes.
+    fn handle_event(&self, event: &Event, repo: &Path) -> Result<(), GitAutoPilotError> {
+        match event.kind {
+            EventKind::Create(_) | EventKind::Modify(_) | EventKind::Remove(_) => {
+                for path in &event.paths {
+                    trace!("Path  - {}", &path.display());
+                    let repo = match Repository::open(repo) {
+                        Ok(repo) => repo,
+                        Err(e) => {
+                            error!("Failed to open repository: {}", e);
+                            continue; // Skip to the next event
+                        }
+                    };
+                    if let Some(ref cred) = self.config.git_credentials {
+                        trace!("Custom user.name: {:#?}", &cred.username);
+                        trace!("Custom user.email: {:#?}", &cred.email);
+                        // Set user configuration (username and email)
+                        let mut config = repo.config()?;
+                        config.set_str("user.name", &cred.username)?;
+                        config.set_str("user.email", &cred.email)?;
+                    }
+                    let git_changes = git::analyze_repository_changes(&repo)?;
+                    if git_changes.is_empty() {
+                        trace!("No git changes found");
+                        continue;
+                    }
+                    debug!("DEBUGPRINT[12]: lib.rs:268: git_changes={:#?}", git_changes);
+                    let file_name = path
+                        .file_name()
+                        .and_then(|name| name.to_str())
+                        .unwrap_or_default();
+                    if let Some(stats) = git_changes
+                        .get(file_name)
+                        // NOTE: in case of rename operation, take first value
+                        .or_else(|| git_changes.values().next())
+                    {
+                        if let Some(file_changes) = stats.first() {
+                            match file_changes.status {
+                                Status::WT_RENAMED => {
+                                    trace!("Rename operation found");
+                                    let _take_git_action = Self::take_action(
+                                        self,
+                                        &repo,
+                                        file_changes,
+                                        git_changes.keys().next().unwrap(),
+                                        &format!(
+                                            "{}/{}",
+                                            path.to_str()
+                                                .unwrap_or_default()
+                                                .split("/")
+                                                .collect::<Vec<&str>>()[..path
+                                                .to_str()
+                                                .unwrap_or_default()
+                                                .split("/")
+                                                .count()
+                                                - 1]
+                                                .join("/"),
+                                            git_changes.keys().next().unwrap()
+                                        ),
+                                    );
+                                }
+                                _ => {
+                                    let _take_git_action = Self::take_action(
+                                        self,
+                                        &repo,
+                                        file_changes,
+                                        file_name,
+                                        path.to_str().unwrap_or(file_name),
+                                    );
+                                }
+                            }
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn take_action(
+        &self,
+        repo: &Repository,
+        file_change_stats: &FileChangeStats,
+        short_file_name: &str,
+        full_file_name: &str,
+    ) -> Result<(), GitAutoPilotError> {
+        debug!(
+            "DEBUGPRINT[10]: lib.rs:317: full_file_name={:#?}",
+            full_file_name
+        );
+        debug!(
+            "DEBUGPRINT[11]: lib.rs:321: short_file_name={:#?}",
+            short_file_name
+        );
+        trace!("{:#?} staging", full_file_name);
+        let repo_branch = git::get_current_branch(repo).unwrap_or("master".to_string());
+        let dynamic_values = Self::prepare_dynamic_values(
+            self,
+            &repo_branch,
+            short_file_name.to_string(),
+            full_file_name.to_string(),
+            file_change_stats,
+        );
+        match file_change_stats.status {
+            Status::WT_NEW | Status::INDEX_NEW => {
+                let _git_stage_file = git::stage_file(&repo, short_file_name, false)?;
+                let (message, description) = get_commit_summary(
+                    dynamic_values,
+                    &self.config.message.create,
+                    &self.config.description.create,
+                );
+                let _git_commit_stagged_change = git::commit(&repo, &message, Some(&description))?;
+                if let Some(git_credentials) = self.config.git_credentials.as_ref() {
+                    let username = git_credentials.login_username.as_ref().unwrap();
+                    let password = git_credentials.password.as_ref().unwrap();
+
+                    git::push(repo, username, password, "origin", &repo_branch)?;
+                } else {
+                    error!("Git credentials are not set");
+                    return Err(GitAutoPilotError::ConfigError(ConfigError::FileError(
+                        "Git credentials are not set".to_string(),
+                    )));
+                }
+            }
+            Status::WT_RENAMED => {
+                let _git_stage_file = git::stage_file(&repo, short_file_name, false)?;
+                if let Some(old_name) = file_change_stats.old_name.as_ref() {
+                    let _git_stage_file = git::stage_file(&repo, old_name, true)?;
+                }
+                let (message, description) = get_commit_summary(
+                    dynamic_values,
+                    &self.config.message.rename,
+                    &self.config.description.rename,
+                );
+                let _git_commit_stagged_change = git::commit(&repo, &message, Some(&description))?;
+                if let Some(git_credentials) = self.config.git_credentials.as_ref() {
+                    let username = git_credentials.login_username.as_ref().unwrap();
+                    let password = git_credentials.password.as_ref().unwrap();
+
+                    git::push(repo, username, password, "origin", &repo_branch)?;
+                } else {
+                    error!("Git credentials are not set");
+                    return Err(GitAutoPilotError::ConfigError(ConfigError::FileError(
+                        "Git credentials are not set".to_string(),
+                    )));
+                }
+            }
+            Status::WT_DELETED => {
+                let _git_stage_file = git::stage_file(&repo, short_file_name, true)?;
+                let (message, description) = get_commit_summary(
+                    dynamic_values,
+                    &self.config.message.remove,
+                    &self.config.description.remove,
+                );
+                let _git_commit_stagged_change = git::commit(&repo, &message, Some(&description))?;
+                if let Some(git_credentials) = self.config.git_credentials.as_ref() {
+                    let username = git_credentials.login_username.as_ref().unwrap();
+                    let password = git_credentials.password.as_ref().unwrap();
+
+                    git::push(repo, username, password, "origin", &repo_branch)?;
+                } else {
+                    error!("Git credentials are not set");
+                    return Err(GitAutoPilotError::ConfigError(ConfigError::FileError(
+                        "Git credentials are not set".to_string(),
+                    )));
+                }
+            }
+            // NOTE: else modified
+            _ => {
+                let _git_stage_file = git::stage_file(&repo, short_file_name, false)?;
+                let (message, description) = get_commit_summary(
+                    dynamic_values,
+                    &self.config.message.modify,
+                    &self.config.description.modify,
+                );
+                let _git_commit_stagged_change = git::commit(&repo, &message, Some(&description))?;
+                if let Some(git_credentials) = self.config.git_credentials.as_ref() {
+                    let username = git_credentials.login_username.as_ref().unwrap();
+                    let password = git_credentials.password.as_ref().unwrap();
+
+                    git::push(repo, username, password, "origin", &repo_branch)?;
+                } else {
+                    error!("Git credentials are not set");
+                    return Err(GitAutoPilotError::ConfigError(ConfigError::FileError(
+                        "Git credentials are not set".to_string(),
+                    )));
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn prepare_dynamic_values(
+        &self,
+        branch: &str,
+        short_file_name: String,
+        full_file_name: String,
+        file_change_stats: &FileChangeStats,
+    ) -> HashMap<String, String> {
+        let mut dynamic_values: HashMap<String, String> = HashMap::new();
+        dynamic_values.insert("BRANCH".to_string(), branch.to_owned());
+        dynamic_values.insert(
+            "STATUS".to_string(),
+            helper::status_to_string(file_change_stats.status),
+        );
+        dynamic_values.insert("FILE_NAME_SHORT".to_string(), short_file_name.to_owned());
+        dynamic_values.insert("FILE_NAME_FULL".to_string(), full_file_name.to_owned());
+        match file_change_stats.status {
+            Status::WT_RENAMED => {
+                dynamic_values.insert(
+                    "FILE_OLD_NAME".to_string(),
+                    file_change_stats
+                        .old_name
+                        .clone()
+                        .unwrap_or_else(|| short_file_name),
+                );
+            }
+            _ => {
+                dynamic_values.insert("FILE_OLD_NAME".to_string(), short_file_name);
+            }
+        }
+        dynamic_values.insert(
+            "DELETIONS".to_string(),
+            file_change_stats.lines_deleted.to_string(),
+        );
+        dynamic_values.insert(
+            "LINES_MODIFIED".to_string(),
+            file_change_stats.lines_modified.to_string(),
+        );
+        dynamic_values.insert(
+            "INSERTIONS".to_string(),
+            file_change_stats.lines_added.to_string(),
+        );
+
+        // Insert system variables into the HashMap
+        for &(key, value) in SYSTEM_VARIABLES {
+            dynamic_values.insert(
+                key.to_string(),
+                byteutils::string::replace_multiple_placeholders(
+                    &format!("{{{{{}}}}}", value),
+                    &dynamic_values,
+                ),
+            );
+        }
+
+        if let serde_json::Value::Object(config_map) = &self.config.variables {
+            for (key, value) in config_map {
+                if let serde_json::Value::String(ref val) = value {
+                    if !dynamic_values.contains_key(key) {
+                        dynamic_values.insert(key.to_string(), val.to_string());
+                    }
+                }
+            }
+        }
+        trace!(
+            "DEBUGPRINT[14]: lib.rs:421: dynamic_values={:#?}",
+            dynamic_values
+        );
+        dynamic_values
     }
 }
 
@@ -227,123 +500,23 @@ fn load_or_create_config(dot_file: &str) -> Result<config::Config, GitAutoPilotE
     }
 }
 
-/// Handles a single file system event by analyzing changes in the corresponding Git repository.
-///
-/// # Arguments
-/// - `event` - The file system event to be handled.
-/// - `repo` - The path to the Git repository related to the event.
-///
-/// # Behavior
-/// - Analyzes repository changes for specified file paths.
-/// - Logs detailed information about the changes.
-fn handle_event(
-    event: &Event,
-    repo: &Path,
-    git_credentials: &Option<GitCred>,
-) -> Result<(), GitAutoPilotError> {
-    match event.kind {
-        EventKind::Create(_) | EventKind::Modify(_) | EventKind::Remove(_) => {
-            for path in &event.paths {
-                trace!("Path  - {}", &path.display());
-                let repo = match Repository::open(repo) {
-                    Ok(repo) => repo,
-                    Err(e) => {
-                        error!("Failed to open repository: {}", e);
-                        continue; // Skip to the next event
-                    }
-                };
-                if let Some(ref cred) = git_credentials {
-                    trace!("Custom user.name: {:#?}", &cred.username);
-                    trace!("Custom user.email: {:#?}", &cred.email);
-                    // Set user configuration (username and email)
-                    let mut config = repo.config()?;
-                    config.set_str("user.name", &cred.username)?;
-                    config.set_str("user.email", &cred.email)?;
-                }
-                let git_changes = git::analyze_repository_changes(&repo)?;
-                if git_changes.is_empty() {
-                    trace!("No git changes found");
-                    continue;
-                }
-                info!("{:#?}", git_changes);
-                let file_name = path
-                    .file_name()
-                    .and_then(|name| name.to_str())
-                    .unwrap_or_default();
-                if let Some(stats) = git_changes
-                    .get(file_name)
-                    // NOTE: in case of rename operation, take first value
-                    .or_else(|| git_changes.values().next())
-                {
-                    if let Some(file_changes) = stats.first() {
-                        match file_changes.status {
-                            Status::WT_RENAMED => {
-                                trace!("Rename operation found");
-                                let _take_git_action = take_action(
-                                    &repo,
-                                    file_changes,
-                                    git_changes.keys().next().unwrap(),
-                                    &format!(
-                                        "{}/{}",
-                                        path.to_str()
-                                            .unwrap_or_default()
-                                            .split("/")
-                                            .collect::<Vec<&str>>()[..path
-                                            .to_str()
-                                            .unwrap_or_default()
-                                            .split("/")
-                                            .count()
-                                            - 1]
-                                            .join("/"),
-                                        git_changes.keys().next().unwrap()
-                                    ),
-                                );
-                            }
-                            _ => {
-                                let _take_git_action = take_action(
-                                    &repo,
-                                    file_changes,
-                                    file_name,
-                                    path.to_str().unwrap_or(file_name),
-                                );
-                            }
-                        }
-                    }
-                } else {
-                    continue;
-                }
-            }
-        }
-        _ => {}
-    }
-    Ok(())
-}
+fn get_commit_summary(
+    dynamic_values: HashMap<String, String>,
+    message: &Message,
+    description: &Message,
+) -> (String, String) {
+    let commit_message = format!(
+        "{}{}{}",
+        byteutils::string::replace_multiple_placeholders(&message.prefix, &dynamic_values),
+        byteutils::string::replace_multiple_placeholders(&message.comment, &dynamic_values),
+        byteutils::string::replace_multiple_placeholders(&message.suffix, &dynamic_values)
+    );
+    let commit_description = format!(
+        "{}{}{}",
+        byteutils::string::replace_multiple_placeholders(&description.prefix, &dynamic_values),
+        byteutils::string::replace_multiple_placeholders(&description.comment, &dynamic_values),
+        byteutils::string::replace_multiple_placeholders(&description.suffix, &dynamic_values)
+    );
 
-fn take_action(
-    repo: &Repository,
-    file_change_stats: &FileChangeStats,
-    short_file_name: &str,
-    full_file_name: &str,
-) -> Result<(), GitAutoPilotError> {
-    debug!(
-        "DEBUGPRINT[10]: lib.rs:317: full_file_name={:#?}",
-        full_file_name
-    );
-    debug!(
-        "DEBUGPRINT[11]: lib.rs:321: short_file_name={:#?}",
-        short_file_name
-    );
-    trace!("{:#?} staging", full_file_name);
-    match file_change_stats.status {
-        Status::WT_RENAMED => {
-            let _git_stage_file = git::stage_file(&repo, short_file_name, false)?;
-            if let Some(old_name) = file_change_stats.old_name.as_ref() {
-                let _git_stage_file = git::stage_file(&repo, old_name, true)?;
-            }
-        }
-        _ => {
-            let _git_stage_file = git::stage_file(&repo, short_file_name, false)?;
-        }
-    }
-    Ok(())
+    (commit_message, commit_description)
 }
